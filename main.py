98a41343a3afe62e82b2ebe08ca622c5f116a59e
Jose Edgar Barroso Neto <96843649+Edgar-Barroso@users.noreply.github.com>
2022-04-22 15:51:57 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,7 @@
 import pygame
-import random
 import cv2
 import numpy as np
 import torch
-from torch import nn, optim
-from torchvision import datasets
-from torchvision import transforms
-from torch.utils.data import DataLoader
-import matplotlib.pyplot as plt
-import time
 import pickle
 
 largura = 400
@@ -32,30 +25,6 @@ else:
   args['device'] = torch.device('cpu')
 
 
-
-
-
-class MLP(nn.Module):
-
-    def __init__(self, input_size, hidden_size, out_size):
-        super(MLP, self).__init__()
-
-        self.features = nn.Sequential(
-            nn.Linear(input_size, hidden_size),
-            nn.ReLU(),
-            nn.Linear(hidden_size, hidden_size),
-            nn.ReLU()
-        )
-        self.out = nn.Linear(hidden_size, out_size)
-        self.softmax = nn.Softmax()
-
-    def forward(self, X):
-        X = X.view(X.size(0), -1)
-        feature = self.features(X)
-        output = self.softmax(self.out(feature))
-        return output
-
-
 def main(frames):
     pygame.init()
     pygame.font.init()
@@ -71,7 +40,7 @@ def main(frames):
                 while pintar:
                     pygame.display.update()
                     x, y = pygame.mouse.get_pos()
-                    pygame.draw.circle(tela, cor_pincel,(x, y), 15)
+                    pygame.draw.circle(tela, cor_pincel,(x, y), 22)
                     for event2 in pygame.event.get():
                         if event2.type == pygame.MOUSEBUTTONUP:
                             pintar = False
@@ -80,24 +49,22 @@ def main(frames):
             if event.type == pygame.QUIT:
                 sair = True
                 break
-
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_DOWN:
                     pygame.image.save(tela, 'tela.jpeg')
                     img = cv2.imread('tela.jpeg')
-                    res = cv2.resize(img, dsize=(28, 28), interpolation=cv2.INTER_CUBIC)
-
+                    res = cv2.resize(img, dsize=(32, 32), interpolation=cv2.INTER_CUBIC)
                     x = []
                     for c in res:
                         for j in c:
-                            x.append(j[0])
+                            x.append(j[0]*0.003)
                     x = torch.Tensor(x).to(args['device'])
-                    x = x.reshape(1, 28, 28)
+                    x = x.reshape(1, 32, 32)
+                    x = x.unsqueeze(1)
                     x = x.to(args['device'])
                     pred = net(x)
                     pred = pred.data.long().numpy()
-                    print(pred)
-                    resultado = np.where(pred == 1)[1][0]
+                    resultado = np.where(pred == pred.max())[1][0]
                     img = font.render(f'{resultado} ', True, 'white')
                     tela.blit(img, (10, 10))
                 if event.key == pygame.K_UP:
@@ -113,6 +80,3 @@ if __name__ == '__main__':
     with open('redeneural.pkl','rb') as f:
         net = pickle.load(f)
     main(frames)
-
-
-
